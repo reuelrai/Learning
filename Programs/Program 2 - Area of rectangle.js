44955a274e758f9c53cfc50400f31dd946ea5a2b
reuel.rai <149403520+reuelrai@users.noreply.github.com>
2024-07-20 14:00:14 +0545

    Add files via upload

---
+++ Programs/Program 2 - Area of rectangle.js
@@ -0,0 +1,8 @@
+function arearect(length, width){
+    return length*width;
+}
+const length = 3;
+const width = 5;
+const area = arearect(length, width);
+
+console.log("Area of Rectangle is: " + area);
